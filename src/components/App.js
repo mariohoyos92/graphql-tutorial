688684ffab7a03305ab6b3044c630f4789674cc8
Mario Hoyos <mariohoyos92@gmail.com>
2018-02-20 10:33:42 -0600

    chore: complete through filtering

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
+import Login from "./Login";
+import Search from "./Search";
 import { Switch, Route } from "react-router-dom";
 
 class App extends Component {
@@ -11,8 +13,10 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
-            <Route exact path="/" component={LinkList} />
+            <Route exact path="/login" component={Login} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/search" component={Search} />
           </Switch>
         </div>
       </div>
